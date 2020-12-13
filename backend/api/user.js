0b24b6d7071cad255d2c6d1bcfe0f286619ce951
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2020-12-13 14:30:25 -0300

    autenticação

---
+++ backend/api/user.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt-nodejs')
+
+module.exports = app => {
+    const obterHash = (pass, callback) => {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(pass, salt, null, (err, hash) => callback(hash))
+        })
+    }
+
+    const save = (req, res) => {
+        obterHash(req.body.pass, hash => {
+            const pass = hash
+
+            app.db('users')
+                .insert({ name: req.body.name, email: req.body.email, pass })
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(400).json(err))
+        })
+    }
+
+    return { save }
+}
--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,14 @@ const app = express()
 const db = require('./config/db')
 const consign = require('consign')
 
-app.get('/', (req, res) => {
-    res.status(200).send('Meu backend')
-})
+consign()
+    // .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
+app.db = db
 
 app.listen(3000, () => {
     console.log('Backend executando...')
